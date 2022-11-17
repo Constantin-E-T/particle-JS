ce5c39773d73e649f8cd448523b2944308e0b411
Emilian Con <emiliancon@ip-10-52-255-199.eu-west-1.compute.internal>
2022-11-17 20:09:02 +0000

    Particles line

--- stars.js
+++ stars.js
@@ -19,7 +19,7 @@ const mouse = {
 canvas.addEventListener('click', function(event){
     mouse.x = event.x;
     mouse.y = event.y;
-    for (let i = 0; i < 7; i++){
+    for (let i = 0; i < 5; i++){
         particlesArray.push(new Particle())
 
     }
@@ -43,6 +43,8 @@ class Particle {
     constructor(){
         this.x = mouse.x;
         this.y = mouse.y;
+        // this.x = Math.random() * canvas.width;
+        // this.y = Math.random() * canvas.height;
         // Change size of particles
         this.size = Math.random() * 20 + 1;
         this.speedX = Math.random() * 3 - 1.5;
@@ -67,33 +69,17 @@ function handleParticles() {
     for (let i = 0; i < particlesArray.length; i++) {
         particlesArray[i].update();
         particlesArray[i].draw();
-        for (let j = i; j < particlesArray.length; j++) {
-            const dx = particlesArray[i].x - particlesArray[j].x;
-            const dy = particlesArray[i].y - particlesArray[j].y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-            if (distance < 100){
-                ctx.beginPath();
-                ctx.strokeStyle = particlesArray[i].color;
-                ctx.lineWidth = .2;
-                ctx.lineWidth = particlesArray[i].size / 10;
-                ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
-                ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
-                ctx.stroke();
-                ctx.closePath();
-            }
-            
-        }
         if (particlesArray[i].size <= 3){
             particlesArray.slice(i, 1);
-            
         }
+        
     }
 }
 
 function animate(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // ctx.fillStyle = 'rgba(0,0,0, .1'
-    // ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgba(0,0,0, .1'
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     handleParticles();
     hue+= 0.5;
     requestAnimationFrame(animate);
